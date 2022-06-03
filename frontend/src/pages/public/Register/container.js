7143d8e759b85323dd118911fd989b17dfae892e
jklimekk <asiaa.klimek@interia.pl>
2022-06-03 13:48:28 +0200

    [KAP-151] create register page

---
+++ frontend/src/pages/public/Register/container.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import RegisterView from './view';
+
+const RegisterContainer = () => {
+  const navigate = useNavigate();
+
+  const onSubmit = async (values, { setFieldError }) => {
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_API_URL}/authentication/signup`,
+        values
+      );
+      navigate('/application/signin');
+    } catch (err) {
+      if (err.response.status === 400) {
+        const error = 'Podany nick, email lub hasło są nieprawidłowe!';
+        setFieldError('username', error);
+        setFieldError('email', error);
+        setFieldError('password', error);
+      }
+    }
+  };
+
+  return <RegisterView onSubmit={onSubmit} />;
+};
+
+export default RegisterContainer;
---
+++ frontend/src/pages/public/Register/view.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Form, Formik } from 'formik';
+import PropTypes from 'prop-types';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import Button from 'components/Button';
+import Input from 'components/Input';
+import validation from './validation';
+
+const RegisterView = ({ onSubmit }) => {
+  const spacing = {
+    marginBottom: 20,
+  };
+
+  return (
+    <Box sx={{ width: 500 }}>
+      <Formik
+        validationSchema={validation}
+        initialValues={{ username: '', password: '', email: '' }}
+        onSubmit={onSubmit}
+        enableReinitialize
+      >
+        <Form>
+          <Typography variant="h1" sx={spacing}>
+            Rejestracja
+          </Typography>
+          <Input name="username" label="Nick" sx={spacing} />
+          <Input name="email" label="E-mail" sx={spacing} />
+          <Input type="password" name="password" label="Hasło" sx={spacing} />
+          <Button submit>Zarejestruj</Button>
+        </Form>
+      </Formik>
+    </Box>
+  );
+};
+
+RegisterView.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default RegisterView;
